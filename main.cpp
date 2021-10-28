85a21bb883cd91e290984248d0c8c5bcf538ca2c
Ayfri <pierre.ayfri@gmail.com>
2021-10-28 14:05:59 +0200

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "SDL.h"
+#include "src/app.cpp"
+#include <utility>
+
+int main(int argc, char *argv[]) {
+	App::init_window();
+	App::setup_renderer();
+	App::quit();
+	
+	long long un
+	
+	return 0;
+}
